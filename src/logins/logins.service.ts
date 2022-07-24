fdc2480da02f4beef5de76965bf73e89a331ae47
Fábio Sabah <fabiosaba@gmail.com>
2022-07-24 16:51:16 -0300

    chore: install jwt pckg

--- src/logins/logins.service.ts
+++ src/logins/logins.service.ts
@@ -4,6 +4,7 @@ import { LoginDto } from './dto/login.dto';
 @Injectable()
 export class LoginsService {
   async login(loginDto: LoginDto): Promise<any> {
+    const { email, password } = loginDto;
     return loginDto;
   }
 }
