a4b82a71f84f697af85077067db6cdac4485f01f
Fábio Sabah <fabiosaba@gmail.com>
2022-07-24 20:43:10 -0300

    feat: create AuthGuard

---
+++ src/common/guards/auth.guard.ts.guard.spec.ts
@@ -0,0 +1,7 @@
+import { Auth.Guard.TsGuard } from './auth.guard.ts.guard';
+
+describe('Auth.Guard.TsGuard', () => {
+  it('should be defined', () => {
+    expect(new Auth.Guard.TsGuard()).toBeDefined();
+  });
+});
